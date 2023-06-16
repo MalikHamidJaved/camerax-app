482a141764e08abf961d37e2fa16e15f81117eb8
Malik hamid javed <mjaved05@i2cinc.com>
2023-06-16 16:24:23 +0500

    State changed

--- app/src/main/java/com/example/cameraxsample/MainActivity.kt
+++ app/src/main/java/com/example/cameraxsample/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.cameraxsample.databinding.ActivityMainBinding
-import com.example.cameraxsample.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
